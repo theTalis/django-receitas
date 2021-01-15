90a193d21b85e20b0d0d96be5332cd85bcd24787
theTalis <cipriani.talis@gmail.com>
2021-01-15 17:11:04 -0300

    commit

---
+++ apps/receitas/views/busca.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from receitas.models import Receita
+
+def busca(request):
+    lista_receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
+    
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+
+    dados = {
+        'receitas' : lista_receitas
+    }            
+
+    return render(request, 'receitas/buscar.html', dados)
